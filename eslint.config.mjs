ffbc48b100caf2fa237cdf674b2d6f8f414a79f3
mabata <marcbataba99@gmail.com>
2024-05-21 11:52:17 +0200

    up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,37 +9,37 @@
 //   ...tseslint.configs.recommended,
 // ];
 
-import pluginJs from "@eslint/js";
-import { Linter } from "eslint";
-import tsEslint from "@typescript-eslint/eslint-plugin";
-import angularEslint from "@angular-eslint/eslint-plugin";
+// import pluginJs from "@eslint/js";
+// import { Linter } from "eslint";
+// import tsEslint from "@typescript-eslint/eslint-plugin";
+// import angularEslint from "@angular-eslint/eslint-plugin";
 
-const config = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  plugins: [
-    '@typescript-eslint',
-    '@angular-eslint',
-  ],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@angular-eslint/recommended',
-  ],
-  rules: {
-    // Ajoutez vos règles spécifiques ici
-  },
-  overrides: [
-    {
-      files: ['*.html'],
-      extends: ['plugin:@angular-eslint/template/recommended'],
-      rules: {
-        // Ajoutez vos règles spécifiques pour les templates ici
-      }
-    }
-  ]
-};
+// const config = {
+//   root: true,
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: './tsconfig.json',
+//   },
+//   plugins: [
+//     '@typescript-eslint',
+//     '@angular-eslint',
+//   ],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:@angular-eslint/recommended',
+//   ],
+//   rules: {
+//     // Ajoutez vos règles spécifiques ici
+//   },
+//   overrides: [
+//     {
+//       files: ['*.html'],
+//       extends: ['plugin:@angular-eslint/template/recommended'],
+//       rules: {
+//         // Ajoutez vos règles spécifiques pour les templates ici
+//       }
+//     }
+//   ]
+// };
 
-export default config;
+// export default config;
