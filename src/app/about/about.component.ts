24e9117d165086ad70b93bfb5487dfe707920fc9
mabata <marcbataba99@gmail.com>
2024-07-11 10:25:08 +0200

    Initial commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  const unusedVa: string = "I am not used";
-
-  // Error: Type 'any' should not be used.
-  const shouldBeStrin: any = "This should be a string";
 
     add(a: number, b: number){
       return a + b;
